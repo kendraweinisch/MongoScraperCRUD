7d03a51977eda67f5e16adc5464ab33be2f22c1c
Kendra Weinisch <kendraweinisch@gmail.com>
2018-07-14 13:12:37 -0700

    updated routes

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -4,15 +4,15 @@ var db = require("../models");
 var scrape = require("../scripts/scrape");
 
 module.exports = {
-  scrapeHeadlines: function(req, res) {
+  scrape: function(req, res) {
     // scrape the NYT
     return scrape()
       .then(function(articles) {
         // then insert articles into the db
         return db.Headline.create(articles);
       })
-      .then(function(dbHeadline) {
-        if (dbHeadline.length === 0) {
+      .then(function(headlinedb) {
+        if (headlinedb.length === 0) {
           res.json({
             message: "No new articles today. Check back tomorrow!"
           });
@@ -20,14 +20,14 @@ module.exports = {
         else {
           // Otherwise send back a count of how many new articles we got
           res.json({
-            message: "Added " + dbHeadline.length + " new articles!"
+            message: "Added " + headlinedb.length + " new articles!"
           });
         }
       })
       .catch(function(err) {
         // This query won't insert articles with duplicate headlines, but it will error after inserting the others
         res.json({
-          message: "Scrape complete!!"
+          message: "Scrape done!!"
         });
       });
   }
--- controllers/headline.js
+++ controllers/headline.js
@@ -8,20 +8,20 @@ module.exports = {
     db.Headline
       .find(req.query)
       .sort({ date: -1 })
-      .then(function(dbHeadline) {
-        res.json(dbHeadline);
+      .then(function(headlinedb) {
+        res.json(headlinedb);
       });
   },
   // Delete the specified headline
   delete: function(req, res) {
-    db.Headline.remove({ _id: req.params.id }).then(function(dbHeadline) {
-      res.json(dbHeadline);
+    db.Headline.remove({ _id: req.params.id }).then(function(headlinedb) {
+      res.json(headlinedb);
     });
   },
   // Update the specified headline
   update: function(req, res) {
-    db.Headline.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true }).then(function(dbHeadline) {
-      res.json(dbHeadline);
+    db.Headline.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true }).then(function(headlinedb) {
+      res.json(headlinedb);
     });
   }
 };
